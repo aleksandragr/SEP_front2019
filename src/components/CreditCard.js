b25bf7f5bd98f8a344612f9731e03fdc930a4397
aleksandragr <grujica995@gmail.com>
2019-01-16 23:48:42 +0100

    fix

--- src/components/CreditCard.js
+++ src/components/CreditCard.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import axios from 'axios';
 
 class CreditCard extends React.Component{
+
+    msbank(){
+
+        axios.get('http://localhost:8051/msbank/bank/getPaymentObj/'+localStorage.getItem('idpayment'))
+        .then(response => 
+            console.log("ok je sve")
+            //window.location.href = response.data
+        )
+    }
 
     render(){
 
@@ -10,7 +20,7 @@ class CreditCard extends React.Component{
                 <div>
                     <p className="p">If you want to pay with {this.props.location.pathname.split('/')[1]}, click on the button below</p>
                     
-                    <Button bsStyle="warning" className="b">Let's start</Button>
+                    <Button onClick={this.msbank()} bsStyle="warning" className="b">Let's start</Button>
                 </div>
 
             </div>
